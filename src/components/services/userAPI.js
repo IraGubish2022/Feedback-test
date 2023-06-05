ac1952e66f8ed211e9fb2b5ca507f0eee8697a17
IraGubish2022 <iragubish@gmail.com>
2023-06-05 15:27:52 +0300

    888

--- src/components/services/userAPI.js
+++ src/components/services/userAPI.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://63a8a4f3f4962215b586468f.mockapi.io';
+//axios.defaults.baseURL = 'https://640865a12f01352a8a91b182.mockapi.io/api/v1/';
 
 export const getAllUsers = async () => {
-  const { data } = await axios('/users');
+  const { data } = await axios.get('/users');
   return data;
 };
 
